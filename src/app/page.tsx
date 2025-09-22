8c97b3cfaa12999f886cc3610db6ec1bb2604d65
Yousef Mohsen <ymohsen746@gmail.com>
2025-09-22 19:05:48 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Slider from "./_components/Slider/Slider";
 import Categories from "./_components/Categories/Categories";
 import AllProducts from "./_components/AllProducts/AllProducts";
 export default async function Home() {
+  console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+  console.log("API:", process.env.API);
   return (
     <section className="py-5 w-[80%] mx-auto">
       <Slider />
